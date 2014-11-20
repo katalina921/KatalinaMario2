f626a892b90325b5b123040d4f8241ac96ce073a
Katalina  Arce <katalinaarce34@gmail.com>
2014-11-20 08:23:05 -0800

    new project

---
+++ public_html/js/screens/play.js
@@ -0,0 +1,28 @@
+game.PlayScreen = me.ScreenObject.extend({
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        // reset the score
+        game.data.score = 0;
+
+        me.levelDirector.loadLevel("Katlevel01");
+
+        var player = me.pool.pull("mario", 0, 420, {});
+        me.game.world.addChild(player, 3);
+
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.LEFT,'left');
+        me.input.bindKey(me.input.KEY.UP,'jump');
+        // add our HUD to the game world
+        this.HUD = new game.HUD.Container();
+        me.game.world.addChild(this.HUD);
+    },
+    /**
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        // remove the HUD from the game world
+        me.game.world.removeChild(this.HUD);
+    }
+});
